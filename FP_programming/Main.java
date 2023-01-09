81cb9ab83c197f7618cd749caa893bc3ca627a38
Alan-Sun1230 <117022253+Alan-Sun1230@users.noreply.github.com>
2023-01-10 03:34:27 +0800

    Update Main.java

--- FP_programming/Main.java
+++ FP_programming/Main.java
@@ -3,21 +3,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Burger cheeseBurger = new Burger(300, 60, cheese);
-		Burger chickenBurger = new Burger(400, 65, chicken);
-		Burger fishBurger = new Burger(390, 75, fish);
+		Burger cheeseBurger = new Burger(300, 60, "cheese");
+		Burger chickenBurger = new Burger(400, 65, "chicken");
+		Burger fishBurger = new Burger(390, 75, "fish");
 
-		Beverage sprite_S = new Beverage(140, 37, S);
-		Beverage sprite_M = new Beverage(200, 42, M);
-		Beverage coke_S = new Beverage(150, 36, S);
-		Beverage coke_M = new Beverage(210, 41, M);
+		Beverage sprite_S = new Beverage(140, 37, 'S');
+		Beverage sprite_M = new Beverage(200, 42, 'M');
+		Beverage coke_S = new Beverage(150, 36, 'S');
+		Beverage coke_M = new Beverage(210, 41, 'M');
 		
-		
-
 		Scanner sc = new Scanner(System.in);
-		
 		
 		sc.close();
 

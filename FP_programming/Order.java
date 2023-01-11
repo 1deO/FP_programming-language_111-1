aaf9afa784217523df10ff798da0694a37d6a558
1deO <117028406+1deO@users.noreply.github.com>
2023-01-11 13:53:44 +0800

    Update Order.java

--- FP_programming/Order.java
+++ FP_programming/Order.java
@@ -1,15 +1,13 @@
 
-import java.util.ArrayList;
+
 public class Order{
 
     private int number;
     private Food food;
-    private ArrayList<Order> orders;
     
     public Order(Food food, int number) {
     	this.food = food;
     	this.number = number;
-    	this.orders = new ArrayList<Order>();
     }
     
 
@@ -29,34 +27,4 @@ public class Order{
     	return food.getCalories() * number;
     }
     
-    public void addOrder(Order order) {
-    	orders.add(order);
-    }
-    
-    public int getOrderCost() {
-    	int sumPrice = 0;
-    	for(Order order : orders) {
-    		sumPrice += order.getTotalPrice();
-    	}
-    	return sumPrice;
-    }
-
-    public int getOrderCalories() {
-    	int sumCalories = 0;
-    	for(Order order : orders) {
-    		sumCalories += order.getTotalCalories();
-    	}
-    	return sumCalories;
-    }
-    
-    public String getInfo() {
-    	String totalOrder = "";
-    	for(Order order : orders) {
-    		totalOrder = totalOrder + order.getFood().getName() + " X " + order.getNumber() 
-    		+ " $ " + order.getTotalPrice() + " " + order.getTotalCalories() + "cal" + "\n";
-    	}
-    	totalOrder = totalOrder + "\n" + "TotalCost: " + getOrderCost() + "\n" + "TotalCalories" + getOrderCalories();
-    	return totalOrder;
-    }
-
 }

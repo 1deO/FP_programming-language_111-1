e6a476a12f728b6ecd1d30bc27f5f07b07eb6330
Alan-Sun1230 <117022253+Alan-Sun1230@users.noreply.github.com>
2023-01-12 23:16:12 +0800

    Update Food.java

--- FP_programming/Food.java
+++ FP_programming/Food.java
@@ -1,14 +1,20 @@
 
 public class Food {
 	
+    private String type;
     private int calories;
     private int price;
     private String name;
 	
-    public Food(String name, int calories, int price){
+    public Food(String type, String name, int calories, int price){
+    	this.type = type;
     	this.name = name;
         this.calories = calories;
         this.price = price;
+    }
+    
+    public String getType() {
+    	return type;
     }
     
     public String getName() {

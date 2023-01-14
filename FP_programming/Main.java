ac21fa3d3a47acb9e602fe06b87e324213bbb052
Alan-Sun1230 <117022253+Alan-Sun1230@users.noreply.github.com>
2023-01-15 02:40:39 +0800

    Update Main.java

--- FP_programming/Main.java
+++ FP_programming/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		//Use constructor to establish the menu(include calories and price)
 		Burger cheeseBurger = new Burger("burger", "CheeseBurger", 300, 60);
 		Burger chickenBurger = new Burger("burger", "ChickenBurger", 400, 65);
 		Burger fishBurger = new Burger("burger", "FishBurger", 390, 75);
@@ -16,18 +17,23 @@ public class Main {
 		Beverage fanta = new Beverage("beverage", "Fanta", 150, 38);
 
 		OrderList orders = new OrderList();
+		
+		//establish an example order
 		Order o1 = new Order(cheeseBurger, 2);
 		Order o2 = new Order(sprite, 1);
 		Order o3 = new Order(fanta, 1);
+		
+		//add every order to the orderlist
 		orders.addOrder(o1);
 		orders.addOrder(o2);
 		orders.addOrder(o3);
 		
 	
-		System.out.println("*Order a hamburger with a beverage can get a discount of 10 dollars off.");
+		System.out.println("*Order a hamburger with a beverage can get a discount of 10 dollars off.");//showing the discount
 		System.out.println("Your order :");
-		System.out.println(orders.getInfo() + "\n");
+		System.out.println(orders.getInfo() + "\n");//print the orders
 		boolean pay = false;
+		//determine whether the input cash is more than orders' cost
 		while (!pay) {
 			System.out.print("Cash:   ");
 			double cash = sc.nextDouble();
@@ -35,7 +41,7 @@ public class Main {
 				System.out.println("Cash is not enough.");
 			}else {
 				pay = true;
-				System.out.println("Change: " + orders.change(cash));
+				System.out.println("Change: " + orders.change(cash));//if true, show the change
 			}
 		}
 		System.out.println("Thank You");
